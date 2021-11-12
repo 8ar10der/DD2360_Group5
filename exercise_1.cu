849275ef56420c8a7768309f75b36fb29851643d
Patrik Zheng <pzheng@kth.se>
2021-11-12 09:46:06 +0100

    Add files via upload

---
+++ exercise_1.cu
@@ -0,0 +1,21 @@
+/* run nvcc -arch-sm_50 code.cu -o -helloworld */
+
+#include <studio.h>
+#define N 1
+/* number of böpcls */
+#define TPB 256
+/* number of threads */
+__global__ void printthreadKernel(){
+  int threadID = threadIdx.x;
+  /* only one block, no need for blockIdx or dim */
+  printf("Hello world! My threadID is %d\n!", threadID);
+  /*print out hello world and ID nr*/
+}
+
+int main(){
+
+  printthreadKernel<<<N, TPB>>>();
+  /* one block, 256 threads */
+  cudaDeviceSynchronize();
+  return (0);
+}
