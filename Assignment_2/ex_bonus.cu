ef5957def5b9bd77149631931739bf8bba441458
Patrik Zheng <pzheng@kth.se>
2021-11-17 14:25:44 +0100

    timed ex_bonus.cu
    added time and controlling total number of trials

--- Assignment_2/ex_bonus.cu
+++ Assignment_2/ex_bonus.cu
@@ -1,25 +1,28 @@
 #include <curand_kernel.h>
 #include <curand.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 /* define threads and blocks */
-#define N 32//blocks = N/TPB
-#define TPB 1024 //threads
-#define TRIALS_PER_THREAD 1000000 //trials per thread
+#define N 32//blocks
+#define TPB 256//threads
+#define TRIALS 1000000 //trials per thread
+
 
 //randomized cointoss
-__global__ void cointoss(curandState *states, unsigned long int* coinTot, unsigned long int* inCirc) {
+__global__ void cointoss(curandState *states, unsigned int* coinTot, unsigned int* inCirc) {
   //setup thread seed
   int id = threadIdx.x + blockDim.x * blockIdx.x;
   int seed = id; //different seed per thread
+  // printf("%d\n", *itpb);
   double piValue = -1;
+  int trls = TRIALS/(N*TPB); //trials per thread
   curand_init(seed, id, 0, &states[id]); //start CURAND
-  for(int i = 0; i < TRIALS_PER_THREAD; i++) {
-    // printf("Line %d, id: %d\n", __LINE__, id);
+  for(int i = 0; i < trls; i++) {
+    //printf("Line %d, id: %d\n", __LINE__, i);
     double x = curand_uniform(&states[id]);
     double y = curand_uniform(&states[id]);
-    // printf("Line %d, id: %d, x: %f, y: %f\n", __LINE__, id, x , y);
-
+    //printf("Line %d, id: %d, x: %f, y: %f\n", __LINE__, id, x , y);
     double throwDist = sqrt(x*x + y*y); //check throw distance
     // printf("Line %d, id: %d, throwDist: %f\n", __LINE__, id, throwDist);
     atomicAdd(coinTot, 1); //increase cointot by 1
@@ -28,30 +31,43 @@ __global__ void cointoss(curandState *states, unsigned long int* coinTot, unsign
       atomicAdd(inCirc, 1); //increase inCirc by 1
     }
     piValue = 4 * (double) *inCirc / (double) *coinTot;
+    // printf("Line %d, id: %d, throwDist: %f\n", __LINE__, id, throwDist);
+    printf("tID: %d, Pi Value %0.10f, iter:%d, cTot:%d \n", id, piValue, i, *coinTot);
+
   }
-  printf(" Pi Value %0.10f, *inCirc: %d, *coinTot:%d \n", piValue, *inCirc, *coinTot);
 
 }
 
-
+__host__ double cpuSecond() {
+   struct timeval tp;
+   gettimeofday(&tp,NULL);
+   return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
 
 int main(){
-  unsigned long int* coinTot; //number of times coin is in circle
-  unsigned long int* inCirc; //number of times coin is in circle
-  
-  cudaMalloc(&coinTot, sizeof(unsigned long int));
-  cudaMalloc(&inCirc, sizeof(unsigned long int));
+  unsigned* coinTot; //number of times coin is thrown
+  unsigned* inCirc; //number of times coin is in circle
+//  int* itpb; //iterations per block
 
-  cudaMemset(coinTot, 0, sizeof(unsigned long int));
-  cudaMemset(inCirc, 0, sizeof(unsigned long int));
+  cudaMalloc(&coinTot, sizeof(unsigned));
+  cudaMalloc(&inCirc, sizeof(unsigned));
+  //cudaMalloc(&itpb, sizeof(int));
 
+  cudaMemset(coinTot, 0, sizeof(unsigned));
+  cudaMemset(inCirc, 0, sizeof(unsigned));
+//  printf("helpÖ %d\n", TRIALS/(N*TPB));
+  //cudaMemset(itpb, (TRIALS/(N*TPB)), sizeof(int));
 
   curandState *dev_random;
   cudaMalloc((void**)&dev_random, TPB*sizeof(curandState));
+  double iStart = cpuSecond();
 
-  cointoss<<<TPB/N, TPB>>>(dev_random, coinTot, inCirc);
+  cointoss<<<N, TPB>>>(dev_random, coinTot, inCirc);
   // generates total of in and out of coinTot
   cudaDeviceSynchronize();
+  double iElaps = cpuSecond() - iStart;
+  int iter = TRIALS;
+  printf("Iterations: %d\nTime elapsed %f s\n", iter, iElaps);
   cudaFree(dev_random);
 
   return 0;
